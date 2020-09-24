5ac342e8a4c1121edd65b9e1d179f037d36beb74
Roy Munoz <royamunoz@gmail.com>
2020-09-24 13:48:03 -0600

    - Se agrega la clase para los gusanos

---
+++ worm.py
@@ -0,0 +1,16 @@
+class Worm:
+    def __init__(self, id, coordinates, luciferin, adaptation, coveredSet, intradistance):
+        self.id = id
+        self.coordinates = coordinates
+        self.luciferin = luciferin
+        self.adaptation = adaptation
+        self.coveredSet = None
+        self.intradistance = intradistance
+        
+     def __str__(self):
+        return (f"Worm:"
+        f"Coordinates: {self.coordinates}"
+        f"Luciferin: {self.luciferin}"
+        f"Adaptation (F): {self.adaptation}"
+        f"Covered data set: {self.coveredSet}"
+        f"Intradistance: {self.}")
