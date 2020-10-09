1b306b553e8e92c905f5fcdea7821b84317bf649
Roy Munoz <royamunoz@gmail.com>
2020-10-08 19:23:52 -0600

    - Se elimina la clase Card

--- card.py
+++
@@ -1,9 +0,0 @@
-class Worm:
-    def __init__(self, suit, rank):
-        self.suit = id
-        self.rank = rank
-        
-    def __str__(self):
-        return (f"Card:"
-        f"Suit: {self.suit}"
-        f"Rank: {self.rank}")
